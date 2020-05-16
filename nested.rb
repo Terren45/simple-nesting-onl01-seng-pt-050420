58226a4ae3546285bd51a43a406f35b6337e5c08
Terren Culberson <culbersonterren@yahoo.com>
2020-05-16 15:41:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,7 +19,8 @@ def hopper
 programmer_hash[:grace_hopper]
 end
 
-def alan_kay_is_known_for { key: "value", key: "value"} 
+def alan_kay_is_known_for 
+  
 	of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
